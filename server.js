8a72d756892f470033b7a3545b69041bd2c39e83
harikanthghanta <harikanth.ghanta@gmail.com>
2017-02-20 11:04:51 -0500

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,14 @@ app.get('/ui/articleone.html', function(req,res){
     res.sendFile(path.join(__dirname,'ui','articleone.html'));
     
 }); 
+app.get('/ui/articletwo.html', function(req,res){
+    res.sendFile(path.join(__dirname,'ui','articletwo.html'));
+    
+}); 
+app.get('/ui/articlethree.html', function(req,res){
+    res.sendFile(path.join(__dirname,'ui','articlethree.html'));
+    
+}); 
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
