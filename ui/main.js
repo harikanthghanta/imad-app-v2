70a7875b55cb24a3be1fff82a967ef73908b15cc
harikanthghanta <harikanth.ghanta@gmail.com>
2017-02-20 15:08:23 -0500

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,8 @@ console.log('Loaded!');
 var element = document.getElementById('main-text');
 
 element.innerHTML = 'New one';
+
+var img = document.getElementById('madi');
+    img.onclick = function () {
+      img.style.marginLeft = '200 px' ;  
+    };
